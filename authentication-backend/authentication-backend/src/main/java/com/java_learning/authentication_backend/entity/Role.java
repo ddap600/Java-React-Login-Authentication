895113801e79eba2b5a79d29397173c703e3b6c3
Ddap <diogopinto728@gmail.com>
2026-08-06 16:58:13 +0000

    created entities

---
+++ authentication-backend/authentication-backend/src/main/java/com/java_learning/authentication_backend/entity/Role.java
@@ -0,0 +1,23 @@
+package com.java_learning.authentication_backend.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "role")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "description")
+    private String description;
+}
---
+++ authentication-backend/authentication-backend/src/main/java/com/java_learning/authentication_backend/entity/User.java
@@ -0,0 +1,35 @@
+package com.java_learning.authentication_backend.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "display_name", nullable = false)
+    private String displayName;
+
+    @Column(name = "username", nullable = false, unique = true)
+    private String username;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    private Set<Role> roles = new HashSet<>();
+}
