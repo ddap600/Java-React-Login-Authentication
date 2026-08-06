3477861ad3b764a2658b8493a326c96b5793e5f6
Ddap <diogopinto728@gmail.com>
2026-08-06 12:45:06 +0000

    initial commit

---
+++ authentication-backend/authentication-backend/src/test/java/com/java_learning/authentication_backend/AuthenticationBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java_learning.authentication_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
