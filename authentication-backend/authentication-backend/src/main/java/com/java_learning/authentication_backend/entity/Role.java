d7ddf3ebfe557bf767ce4a2a557820e32c66d608
Ddap <diogopinto728@gmail.com>
2026-08-07 19:40:49 +0000

    added new constructor

--- authentication-backend/authentication-backend/src/main/java/com/java_learning/authentication_backend/entity/Role.java
+++ authentication-backend/authentication-backend/src/main/java/com/java_learning/authentication_backend/entity/Role.java
@@ -20,4 +20,8 @@ public class Role {
 
     @Column(name = "name")
     private String name;
+
+    public Role(String name) {
+        this.name = name;
+    }
 }
--- authentication-backend/authentication-backend/src/main/java/com/java_learning/authentication_backend/repository/RoleRepository.java
+++ authentication-backend/authentication-backend/src/main/java/com/java_learning/authentication_backend/repository/RoleRepository.java
@@ -1,4 +1,13 @@
 package com.java_learning.authentication_backend.repository;
 
-public interface RoleRepository {
+import com.java_learning.authentication_backend.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+
+    Optional<Role> findByName(String name);
 }
