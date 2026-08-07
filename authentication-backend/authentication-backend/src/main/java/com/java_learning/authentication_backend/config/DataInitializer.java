e40bc08f2f309a0076c2bbed21fb4aecbdf58238
Ddap <diogopinto728@gmail.com>
2026-08-07 19:41:09 +0000

    added data initializer

--- authentication-backend/authentication-backend/src/main/java/com/java_learning/authentication_backend/config/DataInitializer.java
+++ authentication-backend/authentication-backend/src/main/java/com/java_learning/authentication_backend/config/DataInitializer.java
@@ -1,4 +1,46 @@
 package com.java_learning.authentication_backend.config;
 
+import com.java_learning.authentication_backend.entity.Role;
+import com.java_learning.authentication_backend.entity.User;
+import com.java_learning.authentication_backend.repository.RoleRepository;
+import com.java_learning.authentication_backend.repository.UserRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
 public class DataInitializer {
+
+    @Bean
+    CommandLineRunner seedDatabase(
+            UserRepository userRepository,
+            RoleRepository roleRepository,
+            PasswordEncoder passwordEncoder
+    ) {
+        return args -> {
+
+            if (roleRepository.count() == 0) {
+                Role userRole = new Role("USER");
+                Role adminRole = new Role("ADMIN");
+
+                roleRepository.save(userRole);
+                roleRepository.save(adminRole);
+            }
+
+            if (!userRepository.existsByUsername("admin")) {
+
+                Role adminRole = roleRepository.findByName("ADMIN").get();
+
+                User admin = new User();
+                admin.setDisplayName("Admin");
+                admin.setUsername("admin");
+                admin.setPassword(passwordEncoder.encode("password"));
+                admin.getRoles().add(adminRole);
+
+                userRepository.save(admin);
+
+            }
+        };
+    }
 }
