d9e6e91d5fff86ad88cbe60dc13564af9018eef8
Ddap <diogopinto728@gmail.com>
2026-08-07 09:24:53 +0000

    added authentication filter to security config

--- authentication-backend/authentication-backend/src/main/java/com/java_learning/authentication_backend/config/SecurityConfig.java
+++ authentication-backend/authentication-backend/src/main/java/com/java_learning/authentication_backend/config/SecurityConfig.java
@@ -1,6 +1,8 @@
 package com.java_learning.authentication_backend.config;
 
 import com.java_learning.authentication_backend.service.CustomUserDetailsService;
+import com.java_learning.authentication_backend.service.JwtAuthenticationFilter;
+import com.java_learning.authentication_backend.service.JwtService;
 import org.springframework.context.annotation.Configuration;
 
 
@@ -11,14 +13,19 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
 
     private final CustomUserDetailsService customUserDetailsService;
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
-    public SecurityConfig(CustomUserDetailsService customUserDetailsService) {
+    public SecurityConfig(
+            CustomUserDetailsService customUserDetailsService,
+            JwtAuthenticationFilter jwtAuthenticationFilter) {
         this.customUserDetailsService = customUserDetailsService;
+        this.jwtAuthenticationFilter = jwtAuthenticationFilter;
     }
 
 
@@ -32,12 +39,19 @@ public class SecurityConfig {
         // Permit all for Login endpoint
         // Request Authnetication for any other endpoint
 
+        // Before any of this
+        // Check if this request has a JWT
+
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/user").permitAll()
                         .requestMatchers("/api/auth/login").permitAll()
                         .anyRequest().authenticated()
+                )
+                .addFilterBefore(
+                        jwtAuthenticationFilter,
+                        UsernamePasswordAuthenticationFilter.class
                 );
 
         return http.build();
