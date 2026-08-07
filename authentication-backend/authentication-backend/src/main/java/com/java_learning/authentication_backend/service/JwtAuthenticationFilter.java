56bb5ad4e0273cc4edce64c0d70d621d6f963909
Ddap <diogopinto728@gmail.com>
2026-08-07 09:23:22 +0000

    added authentication filter

--- authentication-backend/authentication-backend/src/main/java/com/java_learning/authentication_backend/service/JwtAuthenticationFilter.java
+++ authentication-backend/authentication-backend/src/main/java/com/java_learning/authentication_backend/service/JwtAuthenticationFilter.java
@@ -1,4 +1,35 @@
 package com.java_learning.authentication_backend.service;
 
-public class JwtAuthenticationFilter {
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtService jwtService;
+    private final CustomUserDetailsService customUserDetailsService;
+
+    public JwtAuthenticationFilter(
+            JwtService jwtService,
+            CustomUserDetailsService customUserDetailsService
+    ) {
+        this.jwtService = jwtService;
+        this.customUserDetailsService = customUserDetailsService;
+    }
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain
+    ) throws ServletException, IOException {
+
+        filterChain.doFilter(request, response);
+    }
 }
